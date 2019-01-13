d8379e326e51e37cb46ad712bbe898f21d6fe755
Gabe Horvat <realgabrielhorvat@gmail.com>
2019-01-13 10:38:24 +0100

    fizzbuzz finished

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+def fizzbuzz(number)
+  (1..number).each do |x|
+    if x % 3 == 0 && x % 5 == 0
+      puts "fizzbuzz"
+    elsif x % 3 == 0
+      puts "fizz"
+    elsif x % 5 == 0
+      puts "buzz"
+    else
+      puts x
+    end
+  end
+end
+
+fizzbuzz(70)
